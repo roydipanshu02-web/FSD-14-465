c727a999f0fe806f4e01707bd74f2b12070c70a1
Dipanshu Roy <roydipanshu02@gmail.com>
2026-08-20 14:30:09 +0530

    add App,js v2

--- Lab3/App.js
+++ Lab3/App.js
@@ -1,10 +1,8 @@
 import http from "http";
 
-const server = http.createServer();
-server.on('request',(req,res)=>{
-    res.write("<h1>Welcome to Server Side Programming</h1>");
-    res.write("<h2>Nodemon is Tracking the files</h2>");
-    res.end();
+const server = http.createServer((req,res) => {
+
+    res.end("<h1>Welcome to Server Side</h1>");
 });
 
 server.listen(5000, () => {
